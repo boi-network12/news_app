8b1566c8f9d3e8ac02144fc20f7e094f6e387ae4
okolo kamdilichukwu samuel <150658859+boi-network12@users.noreply.github.com>
2025-02-23 20:44:54 +0100

    .

--- components/NewsList.js
+++ components/NewsList.js
@@ -8,11 +8,21 @@ export default function NewsList({ posts, loading }) {
         return <Text> Loading posts...</Text>
       }
 
-      
+      const calculateScore = (post) => {
+        const likeCount = post.likeCount.length * 5;
+        const createdAt = new Date(post.createdAt);
+    
+        const ageInHours = (Date.now() - createdAt) / (1000 * 60 * 60);
+        const recency = Math.max(50 - ageInHours, 0) * 3;
+    
+        return likeCount + recency;
+      }
+
+      const sortedPosts = posts.slice().sort((a, b) => calculateScore(b) - calculateScore(a));
 
   return (
     <ScrollView>
-        {posts.map((item) => (
+        {sortedPosts.map((item) => (
             <NewsCard 
             key={item._id} 
             title={item.title} 
